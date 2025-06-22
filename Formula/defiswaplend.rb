8e24238d32bffd5476b4f435ff17cc3b69dfa526
ngoxuansuu <ngosuufcx97@gmail.com>
2025-06-23 06:47:26 +0900

    Update to version 1.0.2, add tea.yaml, prepare for npm/Homebrew/pkgx

--- Formula/defiswaplend.rb
+++ Formula/defiswaplend.rb
@@ -2,7 +2,7 @@ class Defiswaplend < Formula
   desc "CLI for deploying DeFiSwapLend smart contract"
   homepage "https://github.com/Ngoxuansuu/DeFiSwapLend"
   url "https://registry.npmjs.org/defiswaplend/-/defiswaplend-1.0.0.tgz"
-  sha256 "162BB1791B86C52ABB3944A571EA2C22DF16E1ACEEE7856CF751C87A98FB53E3"
+  sha256 "4CF61BC40494618B63D64548AAB4776D6836DFA3838A0A24AF7B2EAD1DE05766 "
   license "MIT"
 
   depends_on "node"
